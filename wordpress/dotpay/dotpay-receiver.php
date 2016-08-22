21dd4d54ae99b2d1969e3dbfb9df57be7916f49a
kazlas <kazlas@users.noreply.github.com>
2016-08-22 22:51:18 +0200

    Change pojade_addUser to use get_user_by(..)

--- wordpress/dotpay/dotpay-receiver.php
+++ wordpress/dotpay/dotpay-receiver.php
@@ -3,36 +3,36 @@ require_once ('../wp-load.php');
 require_once ('../wp-content/plugins/user-access-manager/user-access-manager.php');
 
 // define temp user name
-define ( 'USER_EMAIL_ADDRESS', 'user@localhost' );
+define ( 'USER_EMAIL_ADDRESS', 'user@localhost.pl' );
 
 $userData = pojade_addUser ( USER_EMAIL_ADDRESS );
-echo "User: ";
-print_r ( $userData );
 
 $categories = pojade_addUserAccess ( $userData->ID );
-echo "\nCategories: ";
+
+echo "\nCategories for user id: " .  $userData->ID;
 print_r ( $categories );
 
 /**
  * Add user
+ * Based on: https://tommcfarlin.com/create-a-user-in-wordpress/
  */
 function pojade_addUser($email_address) {
-	// https://tommcfarlin.com/create-a-user-in-wordpress/
-	if (null == username_exists ( $email_address )) {
-		
+	$user = get_user_by ( 'login', $email_address );
+	
+	if (! $user) {
 		// Generate the password and create the user
 		$password = wp_generate_password ( 12, false );
 		$user_id = wp_create_user ( $email_address, $password, $email_address );
-		
-		// Set the role
 		$user = new WP_User ( $user_id );
+		
+		// Set the role empty
 		$user->set_role ( '' );
 		
 		// Email the user
 		wp_mail ( $email_address, 'Welcome!', 'Your Password: ' . $password );
-		
-		return $user;
 	}
+	
+	return $user;
 }
 
 /**
@@ -43,9 +43,7 @@ function pojade_addUserAccess($userId, $accessCategoryId = null) {
 	$sObjectType = 'user';
 	
 	$oUserAccessManager = new UserAccessManager ();
-	
 	if (isset ( $userId )) {
-		$oEditUserData = get_userdata ( $userId );
 		$aUserGroupsForObject = $oUserAccessManager->getAccessHandler ()->getUserGroupsForObject ( $sObjectType, $userId );
 	}
 	
